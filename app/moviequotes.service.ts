23332755241a18e18b1ba6ae7f374d6d8b37cf33
David Fisher <fisherds@rose-hulman.edu>
2016-03-21 11:21:37 -0400

    First Firebase read
    This version has two arrays, one per component.  No good. :)

--- app/moviequotes.service.ts
+++ app/moviequotes.service.ts
@@ -1,11 +1,36 @@
 import {Injectable} from "angular2/core";
 import {MOVIEQUOTES} from "./mock_moviequotes";
+import {Moviequote} from "./moviequote.model";
 
 @Injectable()
 export class MovieQuotesService {
 
+
+
+
   getMovieQuotes() {
-    return Promise.resolve(MOVIEQUOTES);
+
+    // TODO: Use Firebase within the Promise
+    return new Promise<Moviequote[]>(resolve => {
+      var mqRef = new Firebase("https://fisherds-movie-quotes.firebaseio.com");
+      var moviequotes = new Array<Moviequote>();
+
+      mqRef.child("quotes").on("child_added", function (snapshot, prevChildKey) {
+        var newMoviequote = snapshot.val();
+        console.log("Got a new quote");
+        console.log(newMoviequote);
+        moviequotes.push(newMoviequote);
+        resolve(moviequotes);
+      });
+
+      // mqRef.child("quotes").on("value", function (snapshot) {
+      //   console.log(snapshot.val());
+      //   resolve([new Moviequote("Test", "quote")]);
+      // });
+
+    });
+
+
   }
 
 }
