10a97a17003321af966aba75bc2951557d22cdec
Tyler Rockwood <rockwotj@gmail.com>
2016-03-22 15:14:20 -0400

    Refactor again

--- app/firebase.service.ts
+++ app/firebase.service.ts
@@ -1,3 +1,4 @@
+///<reference path='./firebase.d.ts'/>
 import {Injectable} from "angular2/core";
 import {Moviequote} from "./moviequote.model";
 // See https://github.com/ReactiveX/rxjs
@@ -5,35 +6,39 @@ import {Observable} from "rxjs/Observable";
 import 'rxjs/add/operator/share';
 
 @Injectable()
-export class MQObservableService {
+export class FirebaseService {
 
-    private ref: Firebase;
-    private quotes: Moviequote[];
-    private source: Observable<Moviequote[]>;
-
-    public subscribe(func) {
-      // http://reactivex.io/rxjs/class/es6/Observable.js~Observable.html#instance-method-subscribe
-      this.source.subscribe(func);     
-    }
-
-    constructor() {
+    /*
+     *  This takes in a FirebaseURL and returns an observable.
+     *
+     *  An example of using this is:
+     *
+     *  FirebaseService.observeArray("https://test.firebaseio.com").subscribe((firebaseArray) =>
+     *    // Do something with the firebaseArray! This callback will 
+     *    // fire with a new array every time a child is
+     *    // added, changed, or removed.
+     *  );
+     *
+     *  If only there was a way to parameterize a method in Typescript...
+     */
+    observeArray(url: string): Observable<any> {
       // For real speed, this should be combined with: 
       // https://facebook.github.io/immutable-js/ see article:
       // http://www.jvandemo.com/how-i-optimized-minesweeper-using-angular-2-and-immutable-js-to-make-it-insanely-fast/
       // Would be fun to speed up, but I don't have time for it :)
-      this.ref = new Firebase("https://fisherds-movie-quotes.firebaseio.com/quotes");
-      this.quotes = [];
+      let ref = new Firebase(url);
+      let quotes = [];
       // https://github.com/ReactiveX/rxjs/blob/master/doc/observable.md
       let observable = Observable.create((observer) => {
-        let listener1 = this.ref.on("child_added", snapshot => {
+        let listener1 = ref.on("child_added", snapshot => {
           let value = snapshot.val();
           value.key = snapshot.key();
           // Cast
-          this.quotes.push(<Moviequote>value);
-          observer.next(this.quotes);
+          quotes.push(value);
+          observer.next(quotes);
         });
-        let listener2 = this.ref.on("child_changed", snapshot => {
-          for (let mq of this.quotes) {
+        let listener2 = ref.on("child_changed", snapshot => {
+          for (let mq of quotes) {
             if (snapshot.key() == mq.key) {
               let newQuote = snapshot.val();
               mq.movie = newQuote.movie;
@@ -41,25 +46,25 @@ export class MQObservableService {
               break;
             }
           }
-          observer.next(this.quotes);
+          observer.next(quotes);
         });
-        let listener3 = this.ref.on("child_removed", snapshot => {
-          for (let i = 0; i < this.quotes.length; i++) {
-            if (this.quotes[i].key == snapshot.key()) {
-              this.quotes.splice(i, 1);
+        let listener3 = ref.on("child_removed", snapshot => {
+          for (let i = 0; i < quotes.length; i++) {
+            if (quotes[i].key == snapshot.key()) {
+              quotes.splice(i, 1);
               break;
             }
           }
-          observer.next(this.quotes);
+          observer.next(quotes);
         });
         // For cleanup
         return () => {
-          this.ref.off("child_added", listener1);
-          this.ref.off("child_changed", listener2);
-          this.ref.off("child_removed", listener3);
+          ref.off("child_added", listener1);
+          ref.off("child_changed", listener2);
+          ref.off("child_removed", listener3);
         };
       });
       // See: http://reactivex.io/rxjs/class/es6/Observable.js~Observable.html#instance-method-share
-      this.source = observable.share();
+      return observable.share();
     }
 }
--- app/quote_table.component.ts
+++ app/quote_table.component.ts
@@ -1,7 +1,7 @@
 import {Component} from "angular2/core";
 import {NgFor} from "angular2/common";
 import {Moviequote} from "./moviequote.model";
-import {MQObservableService} from "./firebase.service";
+import {FirebaseService} from "./firebase.service";
 import {FirebaseEventPipe} from "./firebase.pipe";
 
 @Component({
@@ -11,12 +11,12 @@ import {FirebaseEventPipe} from "./firebase.pipe";
 })
 export class QuoteTableComponent {
   private moviequotes: Array<Moviequote>
-  private firebaseUrl;
-  constructor(private _movieQuotesService: MQObservableService) {
+  private firebaseUrl: string = "https://fisherds-movie-quotes.firebaseio.com/quotes";
+  constructor(private _movieQuotesService: FirebaseService) {
   }
 
   ngOnInit() {
-    this._movieQuotesService.subscribe((moviequotes) => {
+    this._movieQuotesService.observeArray(this.firebaseUrl).subscribe(moviequotes => {
       this.moviequotes = moviequotes;
     });
   }
