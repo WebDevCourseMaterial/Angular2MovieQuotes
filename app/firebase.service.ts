7ef08000b1bdc175fac14d6dfc2095e931b84763
David Fisher <fisherds@rose-hulman.edu>
2016-03-28 11:21:44 -0400

    Storing a FirebaseDataSnapshot array in the service instead of a flat object

--- app/firebase.service.ts
+++ app/firebase.service.ts
@@ -20,7 +20,7 @@ export class FirebaseService {
      *
      *  Next I will add in immutableJS
      */
-    observeArray(url: string): Observable<any> {
+    observeArray(url: string): Observable<FirebaseDataSnapshot[]> {
       // For real speed, this should be combined with: 
       // https://facebook.github.io/immutable-js/ see article:
       // http://www.jvandemo.com/how-i-optimized-minesweeper-using-angular-2-and-immutable-js-to-make-it-insanely-fast/
@@ -30,26 +30,21 @@ export class FirebaseService {
       // https://github.com/ReactiveX/rxjs/blob/master/doc/observable.md
       let observable = Observable.create((observer) => {
         let listener1 = ref.on("child_added", snapshot => {
-          let value = snapshot.val();
-          value.$key = snapshot.key();
-          _array.push(value);
+          _array.push(snapshot);
           observer.next(_array);
         });
         let listener2 = ref.on("child_changed", snapshot => {
-          for (let e of _array) {
-            if (snapshot.key() == e.$key) {
-              let newValue = snapshot.val();
-              for (let prop in newValue) {
-                e[prop] = newValue[prop];
-              }
+          for (var i = 0; i < _array.length; i++) {
+            if (snapshot.key() === _array[i].key()) {
+              _array[i] = snapshot;
               break;
             }
           }
           observer.next(_array);
         });
         let listener3 = ref.on("child_removed", snapshot => {
-          for (let i = 0; i < _array.length; i++) {
-            if (_array[i].$key == snapshot.key()) {
+          for (var i = 0; i < _array.length; i++) {
+            if (_array[i].key() === snapshot.key()) {
               _array.splice(i, 1);
               break;
             }
@@ -66,4 +61,19 @@ export class FirebaseService {
       // See: http://reactivex.io/rxjs/class/es6/Observable.js~Observable.html#instance-method-share
       return observable.share();
     }
+
+  observeObject(url: string): Observable<FirebaseDataSnapshot> {
+    let ref = new Firebase(url);
+    let observable = Observable.create((observer) => {
+      let listener = ref.on("value", snapshot => {
+        observer.next(snapshot);
+      });
+      // For cleanup
+      return () => {
+        ref.off("value", listener);
+      };
+    });
+    // See: http://reactivex.io/rxjs/class/es6/Observable.js~Observable.html#instance-method-share
+    return observable.share();
+  }
 }
