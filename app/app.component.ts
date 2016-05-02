fb5f6644bf64c5e57e273cc4c743ea8ef57c2d9a
David Fisher <fisherds@rose-hulman.edu>
2016-05-02 08:57:01 -0400

    Final attempt before bailing on this approach
    Ended with errors.  This project doesn’t need to finish so bailing here.

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,18 +2,30 @@ import {Component} from "angular2/core";
 import {TitleComponent} from "./title.component";
 import {AddQuoteComponent} from "./add_quote.component";
 import {QuoteTableComponent} from "./quote_table.component";
+import {FIREBASE_PROVIDERS, defaultFirebase, AngularFire, FirebaseListObservable} from "angularfire2/angularfire2";
+import {Observable} from "rxjs/Observable";
+import 'rxjs/Rx';
 
 @Component({
   selector: 'my-app',
   templateUrl: 'app/app.component.html',
   directives: [TitleComponent, AddQuoteComponent, QuoteTableComponent],
+  providers: [FIREBASE_PROVIDERS,
+    defaultFirebase("https://fisherds-movie-quotes.firebaseio.com/quotes")]
 })
 export class AppComponent {
-
-  private firebaseUrl: string = "https://fisherds-movie-quotes.firebaseio.com/quotes";
+  quotesStream: FirebaseListObservable<any[]>;
+  
+  constructor(private af: AngularFire) {
+    this.quotesStream = af.list("/quotes");
+  }
 
   onAddQuoteEvent(mq) {
-    var quotesRef = new Firebase(this.firebaseUrl);
-    quotesRef.push(mq);
+    // var quotesRef = new Firebase(this.firebaseUrl);
+    // quotesRef.push(mq);
+
+    console.log("TODO: Implement.");
+    this.quotesStream.push(mq);
+
   }
 }
--- app/quote_table.component.ts
+++ app/quote_table.component.ts
@@ -1,24 +1,28 @@
 import {Component} from "angular2/core";
 import {Observable} from "rxjs/Observable";
-import {MovieQuotesService} from "./moviequotes.service";
 import {Moviequote} from "./moviequote.model";
+import {AngularFire, FirebaseListObservable} from "angularfire2/angularfire2";
+import 'rxjs/Rx';
 
 @Component({
   selector: 'quote-table',
   templateUrl: 'app/quote_table.component.html',
   inputs: ['firebaseUrl'],
-  providers: [MovieQuotesService]
+  providers: []
 })
 export class QuoteTableComponent {
   
-  private movieQuotesArrayStream: Observable<Moviequote[]>;
-  private firebaseUrl: string;
+  // private movieQuotesArrayStream: Observable<Moviequote[]>;
+
+  private movieQuotesArrayStream: FirebaseListObservable<any[]>;
+  // private firebaseUrl: string;
   
-  constructor(private _movieQuotesService: MovieQuotesService) {
+  constructor(private af: AngularFire) {
+    this.movieQuotesArrayStream = af.list("/quotes");
   }
 
-  ngOnInit() {
-    this.movieQuotesArrayStream = this._movieQuotesService.createMoviequotesStream(
-        this.firebaseUrl);
-  }
+  // ngOnInit() {
+  //   this.movieQuotesArrayStream = this._movieQuotesService.createMoviequotesStream(
+  //       this.firebaseUrl);
+  // }
 }
